85930fa2ff0a56d10d3e98a42277fa038a615f13
Jared Hanson <jaredhanson@gmail.com>
2017-05-26 09:17:28 -0700

    Use new components terminology.

--- app/directory.js
+++ app/directory.js
@@ -7,8 +7,8 @@ exports = module.exports = function(container) {
   
   var factory = new Factory();
   
-  var createImplDecls = container.specs('http://i.bixbyjs.org/ds/createDirectoryImpl');
-  return Promise.all(createImplDecls.map(function(spec) { return container.create(spec.id); } ))
+  var createImplComps = container.components('http://i.bixbyjs.org/ds/createDirectoryImpl');
+  return Promise.all(createImplComps.map(function(comp) { return comp.create(); } ))
     .then(function(impls) {
       impls.forEach(function(impl) {
         factory.use(impl);
